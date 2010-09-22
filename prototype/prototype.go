d06d1e29f7c491595c55be206d16296a9a6e7b11
Matt Kane <mkb-prime@hydrogenproject.com>
2010-09-22 09:00:46 -0400

    some more progress, yay

--- prototype/prototype.go
+++ prototype/prototype.go
@@ -12,6 +12,9 @@ import (
 	"file"
 	"math"
 	"json"
+	"exec"
+	vector "container/vector"
+	ioutil "io/ioutil"
 )
 
 // assume we want stereo, 16-bit output. sample rate is adjustable
@@ -28,28 +31,57 @@ type track []beat
 type config struct {
 	inputlist string
 	bands int
+	sox string
+	soxopts StringVector
 	output io.Writer
 }
 
 type input struct {
 	beats uint
 	beatlength uint
+	channels uint
 }
+
+func openband(config *config, remixspec StringVector, filename string, bandwidth, band int) chan frame {
+	currsoxopts := make(StringVector, 1)
+	currsoxopts.Push("sox")
+	currsoxopts.Push(filename)
+	currsoxopts.AppendVector(config.soxopts)
+	currsoxopts.Push("-")
+	currsoxopts.AppendVector(remixspec)
+	p, err := exec.Run(config.sox, currsoxopts, os.Environ(), os.Getwd(), exec.DevNull, exec.Pipe, exec.DevNull)
+	if err != nil {
+		panic(fmt.Sprintf("couldn't open band %d for reason %s", band, err))
+	}
+	// some day this will use libsox
+	outch := make(chan frame)
+	go func (out chan frame) {	
+		
+	} (outch)
+	return outch
+}
+
+func (config *config) getfileinfo(filename string) (samplelength uint, channels uint) {
+	p, err := exec.Run(config.sox, ["soxi", filename], os.Environ(), os.Getwd(), exec.DevNull, exec.Pipe, exec.Pipe)
+	if err != nil {
+		panic(fmt.Sprintf("couldn't open soxi on file %s! %s", filename, err)
+	}
 	
-
-func makeeffecthandlerstruct(chan frame) C.struct_sox_effects_handler_t {
+	var soxierr, err = ioutil.ReadAll(p.Stderr)
+	if err != nil {
+		panic(fmt.Sprintf("Error reading soxi stderr %s", err))
+	}
+	var soxiout, err = ioutil.ReadAll(p.Stdout)
+	if err != nil {
+		panic(fmt.Sprintf("Error reading soxi stdout %s", err))
+	}
 	
-}
-
-func create(effp *C.struct_sox_effect_t, argc int, argv **C.char) int {
-
-}
-
-func start(effp *C.struct_sox_effect_t) int {
-}
-
-func openband(filename string, band int) chan frame {
-	// this function 
+	err = p.Close()
+	if err != nil {
+		panic(fmt.Sprintf("soxi returned err %s", err))
+	}
+	
+	for line := range strings.Split(soxiout, 
 }
 
 // we need one goroutine per each band to read samples, and one goroutine to read from each channel
@@ -77,7 +109,8 @@ func (config *config) readinputlist() map[string]input {
 		} else if line.HasPrefix("LENGTH ") {
 			_, lengthstr := string.Split(line, " ", 2)
 			info.beats = strconv.Atoui64(lengthstr)
-			info.beatlength = getfilesamples(filename) / info.beats
+			var samplelength, info.channels = config.getfileinfo(filename) / info.beats
+			info.beatlength = samplelength / info.beats
 		}
 	}
 	if in { 
@@ -98,10 +131,12 @@ func (config *config) writeoutput() {
 		}
 		i := 0
 		for band := 0; band < config.bands; band++ {
-			var channel chan frame = openband(filename, band)
-			f := <-channel
-			trackdata[i / beatlength][band].left += fabs(float64(f.left))
-			trackdata[i / beatlength][band].right += fabs(float64(f.right))
+			var channel chan frame = openband(config, filename, band)
+			var f
+			for ; f != nil; f := <-channel {
+				trackdata[i / beatlength][band].left += fabs(float64(f.left))
+				trackdata[i / beatlength][band].right += fabs(float64(f.right))
+			}
 		}
 		tracks[len(tracks)-1] = trackdata
 	}
@@ -115,7 +150,8 @@ func (config *config) writeoutput() {
 
 func (config *config) readflags() {
 	flag.StringVar(&config.inputlist, "inputlist", "inputlist", "list of input files with metadata")
-	flag.IntVar(&config.bands, 20, "bands", "number of bands")
+	flag.IntVar(&config.bands, "bands", 20, "number of bands")
+	flag.StringVar(&config.sox, "sox", "/usr/bin/sox", "Path to sox binary. Default is /usr/bin/sox")
 	o := flag.String("output", "-", "output file. Use \"-\" for stdout.")
 	flag.Parse()
 	if *o == "-" {
@@ -126,6 +162,20 @@ func (config *config) readflags() {
 			panic(fmt.sprintf("couldn't open file %s for output! error %s", *s, err.String()))
 		}
 		config.output = bufio.NewWriter(f)
+	}
+	
+	config.soxopts = make(vector.StringVector, 10)
+	if flag.NArg() > 0 {
+		config.soxopts.AppendVector(flag.Args())
+	} else {
+		config.soxopts.Push("-b")
+		config.soxopts.Push("16")
+		config.soxopts.Push("-e")
+		config.soxopts.Push("signed-integer")
+		config.soxopts.Push("-r")
+		config.soxopts.Push("44100")
+		config.soxopts.Push("-t")
+		config.soxopts.Push("raw")
 	}
 }
 
